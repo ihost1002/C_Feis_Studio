2444b144a1a3b4a459348003154e7c1beff55d3a
ihost1002 <ihost1002@gmail.com>
2024-02-22 01:34:03 +0800

    31_2
    create readString(), printString() to deal with user input String then print them.
    create readString() to read user input.
    create printString() to print user input.

--- 31_2.c
+++ 31_2.c
@@ -5,24 +5,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char *readString(void);
+void printString(const char *);
+
 int main(void) {
+    
+    char *str = readString();
+    printString(str);
+    return 0;
+}
+
+char *readString(void) {
     char *str = 0;
-	int length = 0;
+    int length = 1;
     while (1) {
         char input = 0;
         scanf("%c", &input);
         if (input == '\n') break;
         char *newStr = (char *) malloc(sizeof(char) * (length + 1));
-        for (int i = 0; i < length; i++) newStr[i] = str[i];
+        for (int i = 0; length != 1 && i < length-1; i++) newStr[i] = str[i];
         free(str);
         str= newStr;
-        str[length] = input;
+        str[length-1] = input;
         length++;
     }
-    
-    for (int i = 0; i < length; i++) {
-        printf("%c", str[i]);
+    str[length] = '\0';
+    return str;
+}
+
+void printString(const char * str) {
+    for (char *c = (char *)str; *c != '\0'; c++) {
+        printf("%c", *c);
     }
-    printf("\n");
-    return 0;
 }
