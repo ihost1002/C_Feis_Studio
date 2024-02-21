92a9009dd459ca67e0429ae830af5700466a5ff7
ihost1002 <ihost1002@gmail.com>
2024-02-21 17:46:07 +0800

    cht30
    30_2_1    print array using type (*)[length]
    30_2_2    print array using type (*)[length], height
    30_2_3    print array using type **, heigh, width

---
+++ 30_2_1.c
@@ -0,0 +1,22 @@
+/*
+ * print 2 dimentionl array with type (*) [arraylength]
+ */
+#include <stdio.h>
+
+void print(int (*)[3]);
+
+int main(void) {
+    int v[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+	print(v);
+    return 0;
+}
+
+void print(int (*v)[3]) {
+	int i, j;
+    for (i = 0; i < 3; i ++) {
+        for (j = 0; j < 3; j++) {
+            printf("%d ", v[i][j]);
+        }
+        printf("\n");
+    }
+}
---
+++ 30_2_3.c
@@ -0,0 +1,23 @@
+/*
+ * print 2 dimentionl array with type **, height, width
+ */
+#include <stdio.h>
+
+void print(int **, int, int);
+
+int main(void) {
+    int v[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int *p[3] = {v[0], v[2], v[1]};
+    print(p, 3, 3);
+    return 0;
+}
+
+void print(int **v, int height, int width) {
+    int i, j;
+    for (i = 0; i < height; i++) {
+        for (j = 0; j < width; j++) {
+            printf("%d ", v[i][j]);
+        }
+        printf("\n");
+    }
+}
