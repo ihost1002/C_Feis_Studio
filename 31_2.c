18249642d2403dbbf3b571fba6a0fa5a1c637e24
ihost1002 <ihost1002@gmail.com>
2024-02-23 00:41:24 +0800

    31_2
    using dynamic memory allocation to extend char[][] array size and char[] array size

--- 31_2.c
+++ 31_2.c
@@ -7,11 +7,25 @@
 
 char *readString(void);
 void printString(const char *);
+void printManyStrings(char **, int);
+int isEndOfString(const char *); // true return 1, else return 0
+const char *endString = "END";
 
 int main(void) {
-    
-    char *str = readString();
-    printString(str);
+    char **strings = 0; // store Strings location
+    int length = 0;     // strings array size
+    while (1) {
+        char *str = readString();     // read user's input, end of char is '\n'
+        if (isEndOfString(str)) break;  // end loop while user entered "END"
+        char **largerStr = (char **) malloc(sizeof(char*) * (length + 1)); // reallocate larger string array to store new string's location
+        for (int i = 0; i < length; i++) largerStr[i] = strings[i]; // copy strings
+        free(strings); // free old string location
+        strings = largerStr;  // assign larger string array
+        strings[length] = str; // store new string
+        length++;
+    }
+    printf("------\n");
+    printManyStrings(strings, length);
     return 0;
 }
 
@@ -37,4 +51,24 @@ void printString(const char * str) {
     for (char *c = (char *)str; *c != '\0'; c++) {
         printf("%c", *c);
     }
+    printf(" ");
+}
+
+void printManyStrings(char **str, int length) {
+    for (int i = 0; i < length; i++) {
+        printString(str[i]);
+    }
+    printf("\n");
+}
+
+int isEndOfString(const char *str) {
+    const char *c = str;
+    const char *endStr = endString;
+    int i = 0;
+    while (c[i] != '\0' || endStr[i] !='\0') {
+        if (c[i] != endStr[i]) break;
+        i++;
+    }
+    if (c[i] != '\0' || endStr[i] != '\0') return 0;
+    return 1;
 }
